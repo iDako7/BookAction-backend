abb02b229e9b3656171506628786eb2bfa73d402
dako7777777 <curiosiky@gmail.com>
2025-11-26 22:07:15 -0800

    feat: implement UserRepository for user management
    - Added UserRepository class with methods to find users by email, username, and ID.
    - Implemented user creation and last login update functionalities.
    - Integrated PrismaClient for database operations.

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,52 @@
+import { PrismaClient, User, Prisma } from "../../generated/prisma/client.js";
+import { UserInfoDTO } from "../dtos/request/UserInfoDTO.js";
+
+export class UserRepository {
+  private prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+
+  // Find user by email
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: { email: email.toLowerCase() },
+    });
+  }
+
+  // find user by username
+  async findByUsername(userName: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: { username: userName },
+    });
+  }
+
+  // find user by ID
+  async findByUserid(userId: number): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+  }
+
+  // create new user
+  async create(userinfo: UserInfoDTO): Promise<User> {
+    // validation of user_info shall be already finished in service layer, since this layer only focus on DB related logic
+
+    return await this.prisma.user.create({
+      data: {
+        email: userinfo.email,
+        username: userinfo.username,
+        password_hash: userinfo.passwordHash,
+      },
+    });
+  }
+
+  // update last login
+  async updateLogin(userId: number): Promise<void> {
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { last_login: new Date() },
+    });
+  }
+}
